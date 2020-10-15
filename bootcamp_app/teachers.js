e07a011d308bfff62699c439ddaffe28d3159d5d
Daya <dayanayak30@gmail.com>
2020-10-15 00:28:07 +0000

    SQL Injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -13,16 +13,18 @@ if (!cohortName) {
   console.log("Please provide cohort name");
   return;
 }
+const queryString = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers 
+  JOIN assistance_requests ON teachers.id = teacher_id
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name = $1
+  ORDER BY teachers.name;
+  `;
+const values = [`${cohortName}`];
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers 
-JOIN assistance_requests ON teachers.id = teacher_id
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${cohortName}'
-ORDER BY teachers.name;
-`)
+pool.query(queryString,values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
